ecd8c87e0a2c45a4e53f78ea349ace684003f1a4
jo <djh1997@hotmail.com>
2024-03-15 11:47:00 +0000

    correct offset values

--- temp.sh
+++ temp.sh
@@ -4,9 +4,10 @@ OF=temp.svg
 templist=$(tail -1440 tempstress.log)
 temparr=( $templist )
 temparrlength=(${#temparr[@]})
+svgwidth=$(($temparrlength*5))
 
 #make the graph background 
-echo '<svg height="500" width="'$temparrlength'"><defs>
+echo '<svg height="500" width="'$svgwidth'"><defs>
     <linearGradient id="grad1" x1="0%" y1="0%" x2="0%" y2="100%">
       <stop offset="0%"
       style="stop-color:rgb(255,80,80)" />
@@ -16,7 +17,7 @@ echo '<svg height="500" width="'$temparrlength'"><defs>
       style="stop-color:rgb(80,255,80)" />
     </linearGradient>
   </defs>
-  <rect width="'$temparrlength'" height="500" fill="url(#grad1)" /><path d="' > $OF
+  <rect width="'$svgwidth'" height="500" fill="url(#grad1)" /><path d="' > $OF
 #make vertical time axis
 for (( i=60; i<1400; i+=60 )); do
  echo 'M'$i' 0L'$i' 500' >> $OF ;
@@ -25,7 +26,7 @@ done
 echo 'M0 166L1440 166M0 333L1440 333Z"stroke="white"/><polyline points="' >> $OF
 #iterate over temperature array to plot point
 for (( i=0; i<$temparrlength; i++ )); do
- echo "$i,$((1000-${temparr[$i]}*10)) ">> $OF ;
+ echo "$(($i*5)),$((750-${temparr[$i]}/100)) ">> $OF ;
 done
 
 echo '"style="fill:none;stroke:black;stroke-width:1" /><polyline points="' >> $OF
